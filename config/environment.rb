4566fb8359e61a3d8a1dabd1ef145d5a98b59495
NikolaBalog <offinger@gmail.com>
2012-12-17 16:38:10 +0100

    db3

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ActiveSupport::Deprecation.silence do
-  Tgrouo::Application.initialize!
+  Tgroup::Application.initialize!
 end
 
 
